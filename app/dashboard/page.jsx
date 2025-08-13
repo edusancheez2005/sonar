4cb0297940f6b26a2aa50c169bbe4899339f1781
Eduardo Sanchez Morales <eduardosanchez4848@gmail.com>
2025-08-13 15:24:17 +0100

    applied fixes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import Dashboard from '@/src/views/Dashboard'
 import AuthGuard from '@/app/components/AuthGuard'
--- app/statistics/page.jsx
+++ app/statistics/page.jsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import Statistics from '@/src/views/Statistics'
 import AuthGuard from '@/app/components/AuthGuard'
