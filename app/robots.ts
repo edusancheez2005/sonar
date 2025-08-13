03643b3c39e2b5dedbc5b482bb5dd7f280ac97bd
Eduardo Sanchez Morales <eduardosanchez4848@gmail.com>
2025-08-13 21:16:11 +0100

    fix(seo): create new sitemap-main.xml to bypass GSC cache issues

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,6 @@ export default function robots(): MetadataRoute.Robots {
   const base = 'https://www.sonartracker.io'
   return {
     rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: `${base}/sitemap.xml`,
+    sitemap: `${base}/sitemap-main.xml`,
   }
 } 
