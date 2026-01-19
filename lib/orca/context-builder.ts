c7407cd14c8e67f00a993de01a3d4766bde6a528
Eduardo Sanchez Morales <eduardosanchez4848@gmail.com>
2026-01-19 00:46:55 +0000

    Fix: remove duplicate formatLargeNumber function

--- lib/orca/context-builder.ts
+++ lib/orca/context-builder.ts
@@ -816,13 +816,6 @@ function processWhaleAlertsData(alertsData: any[]): WhaleAlertsData {
   }
 }
 
-function formatLargeNumber(num: number): string {
-  if (num >= 1e9) return `$${(num / 1e9).toFixed(2)}B`
-  if (num >= 1e6) return `$${(num / 1e6).toFixed(2)}M`
-  if (num >= 1e3) return `$${(num / 1e3).toFixed(2)}K`
-  return `$${num.toFixed(2)}`
-}
-
 /**
  * Process sentiment data
  */
