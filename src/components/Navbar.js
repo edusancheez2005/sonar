31922535e1c54701ce3b268f0be1067a36666b0c
Eduardo Sanchez Morales <t-eduardos@microsoft.com>
2026-02-20 11:10:26 +0000

    nav: add Leaderboard link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -262,6 +262,11 @@ const Navbar = ({ onLogout }) => {
               </MenuItem>
             </motion.div>
             <motion.div variants={menuVariants} initial="hidden" animate="visible">
+              <MenuItem $active={isActive('/whales/leaderboard')} variants={itemVariants}>
+                <NextLink href="/whales/leaderboard">Leaderboard</NextLink>
+              </MenuItem>
+            </motion.div>
+            <motion.div variants={menuVariants} initial="hidden" animate="visible">
               <MenuItem $active={isActive('/trending')} variants={itemVariants}>
                 <NextLink href="/trending">Trending</NextLink>
               </MenuItem>
